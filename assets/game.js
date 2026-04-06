cad633efcc3055ea409f799a17f40015e009195b
Kafa Sallayan Amca (Ali) <104275912+AliGqmrr@users.noreply.github.com>
2026-04-06 21:24:01 +0300

    Comment out blackDiv functionality in game.js
    Comment out the creation and usage of blackDiv for splash screen.

--- assets/game.js
+++ assets/game.js
@@ -50,7 +50,7 @@ window.onload = function () {
     musicButton.src = "https://hop-run.vercel.app/assets/musicoff.png";
   }
 
-  const blackDiv = document.createElement('div');
+ /* const blackDiv = document.createElement('div');
   blackDiv.style.position = 'fixed';
   blackDiv.style.inset = '0';
   blackDiv.style.backgroundColor = 'black';
@@ -99,7 +99,7 @@ window.onload = function () {
   }
 
   blackDiv.addEventListener('click', () => removeSplash());
-  setTimeout(() => removeSplash(), 5000);
+  setTimeout(() => removeSplash(), 5000);*/
 };
 
 function setLocalStorage(cname, cvalue, exdays) {
